ec78dc303cad16e5f8abd5d8abcf2d967a7a62ba
Haley-Hartin <55203391+Haley-Hartin@users.noreply.github.com>
2019-11-04 19:07:14 -0700

    Update restaurant.js

--- Shelf_life/resources/js/restaurant.js
+++ Shelf_life/resources/js/restaurant.js
@@ -32,3 +32,13 @@ var select = 'SELECT * FROM USERS;';
 		.catch(function(err){
 			console.log("error message",err);
 		});
+INSERT INTO INVENTORY(Ingredient_ID, Ingredient_Group_ID,Ingredient_Name,Ingredient_Quantity,Ingredient_Cost,Ingredient_Unit)
+INSERT INTO INVENTORY(Ingredient_ID, Ingredient_Group_ID,Ingredient_Name,Ingredient_Quantity,Ingredient_Cost,Ingredient_Unit)
+VALUES (1,2,'Cheese',30,0.5,'oz'),(2,1,'steak',15,2,'oz'),(3,3,'bread',5,1,'loaf'),(4,3,'pasta',20,0.1,'oz');
+
+// filling Dish_2_Ingredients
+INSERT INTO Dish_2_Ingredients VALUES(1,(SELECT ingredient_id FROM INVENTORY WHERE ingredient_name='pasta'),5);
+//summing up each ingredient used in dish
+SELECT COUNT(ingredient_quanity) FROM Dish_2_Ingredients WHERE dish_id=1 and ingredient_id=1;
+//update inventory
+UPDATE inventory SET ingredient_quantity=(SELECT ingredient_quantity FROM inventory WHERE ingredient_id=1)-(SELECT ingredient_quanity FROM dish_2_ingredients WHERE dish_id=1 and ingredient_id=1) WHERE ingredient_id=1;
